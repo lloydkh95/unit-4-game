524111294ecb0055598977de7d87516ef2b2ae6f
lloydkh95 <lloydkh95@gmail.com>
2019-08-06 13:30:14 -0400

    reworked the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,120 @@
-var random_results;
-var lost = 0;
-var win = 0;
-var previous = 0:
-
-var resetAndStart = function () {
-    $(".crystals").empty();
-    var images = []}
-    random_result = Math.floor(Math.random() * 69) + 30;
-    $("result").html("Random Result: " + random_result);
-    for(var i = 0; i < 4; i++){
-        var random = Math.floor(Math.random() * 11) + 1;
-        var crystal = $("<div>");
-        crystal.attr({
-            "class": "crystal",
-            "data-random": random
-        });
-        crystal.css({
-            "backgroung-image":"url"( + images[1] + ),
-            "background-size": "cover"
-        });
-
-        $(".crystals").append(crystal);
+// GLOBAL variables 
+//CRYSTAL variables 
+var crystal = {
+    blue:
+    {
+        name:"Blue",
+        value: 0
+    },
+    green:
+    {
+        name: "Green",
+        value: 0
+    },
+    red:
+    {
+        name: "Red",
+        value: 0
+    },
+    yellow:
+    {
+        name: "Yellow",
+        value: 0
     }
-    $("#previous").html("Total Score:" + previous);
+};
+//Scores (Current Target)
+var currentScore = 0;
+var targetScore  = 0;
+//wins and loses
+var winCount = 0;
+var lossCount = 0;
+//Functions
 
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-resetAndStart();
+var startGame = function() {
+    // Reset the current score
+    var currentScore = 0;
+    // Set a new Taarget Score
+    targetScore = getRandom(19, 120);
+    // Set different values for each of the crystals
+    crystal.blue.value  = getRandom(1, 12);
+    crystal.red.value   = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+    // change the HTMLto reflect all of these changes
+    $("#yourScore").html(currentScore);
+    $("#targetScore").html(targetScore);
+    //testing console
+    console.log("---------------------------")
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value + " | Yellow: " + crystal.yellow.value)
+    console.log("---------------------------")
+}
+//Respond to clicks on the crystals
+var addValues = function(crystal) {
+    //change current score
+    currentScore = currentScore + crystal.value;
 
-$(document).on('click', "")
+    // change the html to reflect changes in current score
+    $("#yourScore").html(currentScore);
 
+    //call the checking function
+    checkWin();
 
-// psuedo coding
-// a game with 4 crystals and a random result
-// every crystal needs to have a random number between 1-12
-// a new random number should be generated every single time we win or lose 
-// to those 4 crystals
-// when clicking any crystal it should add the previous result
-// until it equals the final score
-// if it is not equal then we start over
-// if it is equal then we increment a win counter
+    // testing console
+    console.log("Your Score: " + currentScore);
+}
+// Main process
+var checkWin = function() {
+
+    // check if currentScore is larger than the targetScore
+    if(currentScore > targetScore) {
+        alert("Sorry. You lost");
+        console.log("You lost");
+
+        // add to loss Counter
+        lossCount++;
+        
+        // change loss count html
+        $("#lossCount").html(lossCount);
+
+        // Restart the game
+        startGame();
+    }
+
+    else if (currentScore == targetScore) {
+        alert("Congradulations! You Won!");
+        console.log("You Won!");
+
+        //add the win counter
+        winCount++;
+
+        // Change win count html
+        $("#winCount").html(winCount);
+
+        //Restart the game
+        startGame();
+    }
+}
+
+// Main process
+startGame();
+
+$("#blue").click(function(){
+    addValues(crystal.red);
+});
+
+$("#red").click(function() {
+    addValues(crystal.red);
+});
+
+$("#green").click(function(){
+    addValues(crystal.green);
+});
+
+$("#yellow").click(function(){
+    addValues(crystal.yellow);
+});
